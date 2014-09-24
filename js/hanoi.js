cd714b9473737cf1bed8dc38c113510842131067
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2014-09-24 20:16:05 +0200

    added color distribution calculation added build scripts

--- js/hanoi.js
+++ js/hanoi.js
@@ -1,9 +1,10 @@
 (function() {
+    'use strict';
+
     var ImgData = function(img) {
         // Create canvas
-        $('body')
-            .remove('#dummyCanvas')
-            .append('<canvas id="dummyCanvas"></canvas>');
+        $('body #dummyCanvas').remove();
+        $('body').append('<canvas id="dummyCanvas"></canvas>');
         var $dummyCanvas = $('#dummyCanvas', this.$elem);
         // Canvas dimensions should match image dimensions
         $dummyCanvas
@@ -33,6 +34,62 @@
         //imageData.data type is CanvasPixelArray  4*width*height
     };
 
+    ImgData.prototype.statistics = function() {
+        var dominance = this.calculateColorDominance();
+        console.log('color dominance: ', dominance);
+        var totalPixels = this.imgData.data.length / 4;
+        this.drawDominance(dominance, totalPixels);
+    };
+
+    ImgData.prototype.calculateColorDominance = function() {
+        // Color dominance RGB
+        var result = [0, 0, 0, 0]; // [R, G, B, neither]
+        var data = this.imgData.data;
+        for(var i = 0; i < data.length; i = i + 4) {
+            if(i % 100000 === 0) {
+                console.log(i);
+            }
+            var r = data[i];
+            var g = data[i+1];
+            var b = data[i+2];
+            var highest = Math.max(r,g,b);
+            // If one color is higher than the others, add one point to dominance result
+            if(r === highest && g !== highest && b !== highest) {
+                result[0] = result[0] + 1;
+            } else if(g === highest && r !== highest && b !== highest) {
+                result[1] = result[1] + 1;
+            } else if(b === highest && g !== highest && r !== highest) {
+                result[2] = result[2] + 1;
+            } else {
+                result[3] = result[3] + 1;
+            }
+        }
+        return result;
+    };
+
+    ImgData.prototype.drawDominance = function(dominance, totalPixels) {
+        // TODO benchmarking of functions
+        // TODO colordominance to class/util
+        // TODO clean up can be better
+        // TODO rename to color distribution
+        // TODO better templating/constructing
+        // TODO expand to color histogram: a representation of the distribution of colors in an image. For digital images, a color histogram represents the number of pixels that have colors in each of a fixed list of color ranges, that span the image's color space, the set of all possible colors.
+        //      http://www.kenrockwell.com/tech/histograms.htm
+        $('.distribution').empty();
+        var $stats = $('<div id="colorDominance"></div>');
+        var redPrc = Math.round((dominance[0] / totalPixels) * 100);
+        var greenPrc = Math.round((dominance[1] / totalPixels) * 100);
+        var bluePrc = Math.round((dominance[2] / totalPixels) * 100);
+        var neitherPrc = Math.round((dominance[3] / totalPixels) * 100);
+        $stats.append('<div class="red" title="red: ' + redPrc + '%" style="height: ' + redPrc + '%;"></div>');
+        $stats.append('<div class="green" title="green: ' + greenPrc + '%" style="height: ' + greenPrc + '%;"></div>');
+        $stats.append('<div class="blue" title="blue: ' + bluePrc + '%" style="height: ' + bluePrc + '%;"></div>');
+        $stats.append('<div class="neither" title="neither: ' + neitherPrc + '%" style="height: ' + neitherPrc + '%;"></div>');
+        $('.distribution')
+            .append('<h1>color distribution</h1>')
+            .append($stats);
+    };
+
     /* #reader Object start */
     var Reader = function($elem) {
         this.$elem = $elem;
@@ -49,7 +106,7 @@
     Reader.prototype.bindFileInput = function() {
         var self = this;
         self.$input.change(function() {
-            var file = $(this)[0].files[0];
+            //var file = $(this)[0].files[0];
             self.$preview.empty();
             $.each($(this)[0].files, function(index, file) {
                 if(isImage(file)) {
@@ -85,7 +142,7 @@
     };
 
     Reader.prototype.readImageBinary = function(image) {
-        var self = this;
+        //var self = this;
 
         var reader = new FileReader();
         reader.onload = function() {
@@ -95,6 +152,7 @@
             img.onload = function() {
                 var imgData = new ImgData(this);
                 imgData.log();
+                imgData.statistics();
             };
         };
         reader.readAsDataURL(image);
